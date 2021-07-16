37ed36f2a8407cd61c25f624933e5771994d9538
dpasionek <dante.pasionek1@gmail.com>
2021-07-15 21:42:38 -0600

    Added basic Parrot command executor

---
+++ plugin/src/main/java/dev/joemedina/parrot/commands/ParrotCommand.java
@@ -0,0 +1,41 @@
+package dev.joemedina.parrot.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class ParrotCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] args) {
+        if(!(commandSender instanceof Player))
+        {
+            commandSender.sendMessage("This command can only be execute by a player!");
+            return false;
+        }
+
+        Player player = (Player) commandSender;
+
+        if(args.length > 0)
+        {
+            switch(args[1].toLowerCase())
+            {
+                case "list": ParrotCommand(player, args);
+                case "help": HelpCommand(player);
+                default: HelpCommand(player);
+            }
+        }
+        return false;
+    }
+
+    private void HelpCommand(Player player)
+    {
+
+    }
+
+    private void ParrotCommand(Player player, String[] args)
+    {
+
+    }
+}
