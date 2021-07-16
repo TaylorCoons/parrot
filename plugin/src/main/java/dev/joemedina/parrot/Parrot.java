a19965fc850b81ce968353b9354e70111ad4c109
dpasionek <dante.pasionek1@gmail.com>
2021-07-15 21:32:52 -0600

    Initial commit of JavaPlugin

---
+++ plugin/src/main/java/dev/joemedina/parrot/Parrot.java
@@ -0,0 +1,15 @@
+package dev.joemedina.parrot;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Parrot extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+}
